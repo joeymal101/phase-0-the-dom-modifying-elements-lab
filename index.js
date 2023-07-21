3b18ffc08c4026a4431fb07c067c6321a98fce51
Joell Malone <joellmalone000@gmail.com>
2023-07-21 02:08:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.getElementById('main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Joell is the champion'
